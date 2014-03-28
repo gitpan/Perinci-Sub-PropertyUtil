04014c2d0ad2e4e83ccd1a3078f59eb5e40096ff
Steven Haryanto <stevenharyanto@gmail.com>
2014-03-28 15:23:12 +0000

    Import of SHARYANTO/Perinci-Sub-PropertyUtil-0.02 from CPAN.
    gitpan-cpan-distribution: Perinci-Sub-PropertyUtil
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SHARYANTO/Perinci-Sub-PropertyUtil-0.02.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Perinci/Sub/PropertyUtil.pm
+++ lib/Perinci/Sub/PropertyUtil.pm
@@ -1,6 +1,6 @@
 package Perinci::Sub::PropertyUtil;
 
-use 5.010;
+use 5.010001;
 use strict;
 use warnings;
 
@@ -10,7 +10,7 @@ our @EXPORT_OK = qw(
                        declare_property
                );
 
-our $VERSION = '0.01'; # VERSION
+our $VERSION = '0.02'; # VERSION
 
 sub declare_property {
     my %args   = @_;
@@ -18,44 +18,54 @@ sub declare_property {
     my $schema = $args{schema} or die "Please specify property's schema";
     my $type   = $args{type};
 
+    $name =~ m!\A((result)/)?\w+\z! or die "Invalid property name";
+
     my $bs; # base schema (Rinci::metadata)
     my $ts; # per-type schema (Rinci::metadata::TYPE)
     my $bpp;
     my $tpp;
 
-    require Rinci::Schema;
-    $bs = $Rinci::Schema::base;
-    $bpp = $bs->[1]{"keys"}
-        or die "BUG: Schema structure changed (1)";
-    $bpp->{$name}
-        and die "Property '$name' is already declared in base schema";
-    if ($type) {
-        if ($type eq 'function') {
-            $ts = $Rinci::Schema::function;
-        } elsif ($type eq 'variable') {
-            $ts = $Rinci::Schema::variable;
-        } elsif ($type eq 'package') {
-            $ts = $Rinci::Schema::package;
-        } else {
-            die "Unknown/unsupported property type: $type";
-        }
-        $tpp = $ts->[1]{"[merge+]keys"}
-            or die "BUG: Schema structure changed (1)";
-        $tpp->{$name}
-            and die "Property '$name' is already declared in $type schema";
-    }
-    ($bpp // $tpp)->{$name} = $schema;
+    # insert the property's schema into Rinci::Schema's data
+    {
+        # XXX currently we skip result/*
+        last if $name =~ m!\Aresult/!;
 
+        require Rinci::Schema;
+        $bs = $Rinci::Schema::base;
+        $bpp = $bs->[1]{"keys"}
+            or die "BUG: Schema structure changed (1)";
+        $bpp->{$name}
+            and die "Property '$name' is already declared in base schema";
+        if ($type) {
+            if ($type eq 'function') {
+                $ts = $Rinci::Schema::function;
+            } elsif ($type eq 'variable') {
+                $ts = $Rinci::Schema::variable;
+            } elsif ($type eq 'package') {
+                $ts = $Rinci::Schema::package;
+            } else {
+                die "Unknown/unsupported property type: $type";
+            }
+            $tpp = $ts->[1]{"[merge+]keys"}
+                or die "BUG: Schema structure changed (2)";
+            $tpp->{$name}
+                and die "Property '$name' is already declared in $type schema";
+        }
+        ($bpp // $tpp)->{$name} = $schema;
+    }
+
+    # install wrapper
     {
         require Perinci::Sub::Wrapper;
         no strict 'refs';
+        my $n = $name; $n =~ s!/!__!g;
         if ($args{wrapper}) {
-            *{"Perinci::Sub::Wrapper::handlemeta_$name"} =
+            *{"Perinci::Sub::Wrapper::handlemeta_$n"} =
                 sub { $args{wrapper}{meta} };
-            *{"Perinci::Sub::Wrapper::handle_$name"} =
+            *{"Perinci::Sub::Wrapper::handle_$n"} =
                 $args{wrapper}{handler};
         } else {
-            *{"Perinci::Sub::Wrapper::handlemeta_$name"} =
+            *{"Perinci::Sub::Wrapper::handlemeta_$n"} =
                 sub { {} };
         }
     }
@@ -64,9 +74,11 @@ sub declare_property {
 1;
 # ABSTRACT: Utility routines for Perinci::Sub::Property::* modules
 
-
 __END__
+
 =pod
+
+=encoding UTF-8
 
 =head1 NAME
 
@@ -74,7 +86,7 @@ Perinci::Sub::PropertyUtil - Utility routines for Perinci::Sub::Property::* modu
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =head1 SYNOPSIS
 
@@ -88,16 +100,31 @@ L<Perinci>
 
 Perinci::Sub::Property::* modules.
 
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Perinci-Sub-PropertyUtil>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-Perinci-Sub-PropertyUtil>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Perinci-Sub-PropertyUtil>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
 =head1 AUTHOR
 
 Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2012 by Steven Haryanto.
+This software is copyright (c) 2014 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
-
